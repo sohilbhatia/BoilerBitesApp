d9a13f762cdd70a7d8b06ccf71c789cc8cf6a7ac
Sohil Bhatia <bhatiasohil@hotmail.com>
2023-09-16 14:33:48 -0400

    added navigation

--- BoilerBites/ContentView.swift
+++ BoilerBites/ContentView.swift
@@ -9,11 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Section(header: Text("Test").font(.title)) {
-            Text("Blablabla")
+        NavigationView {
+            List {
+                NavigationLink(destination: DetailView(title: "Hillenbrand")) {
+                    Text("Hillenbrand")
+                }
+                NavigationLink(destination: DetailView(title: "Wiley")) {
+                    Text("Wiley")
+                }
+                NavigationLink(destination: DetailView(title: "Windsor")) {
+                    Text("Windsor")
+                }
+            }
+            .navigationTitle("Your Dining Courts")
         }
     }
 }
+
+struct DetailView: View {
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.largeTitle)
+            .navigationBarTitle(title)
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
