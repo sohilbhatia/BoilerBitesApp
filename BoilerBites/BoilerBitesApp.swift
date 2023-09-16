cb74471879475022e728492c63b48ec75b6345b4
Sohil Bhatia <bhatiasohil@hotmail.com>
2023-09-16 13:45:45 -0400

    Initial Commit

---
+++ BoilerBites/BoilerBitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoilerBitesApp.swift
+//  BoilerBites
+//
+//  Created by Sohil Bhatia on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BoilerBitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoilerBites/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  BoilerBites
+//
+//  Created by Sohil Bhatia on 9/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Section(header: Text("Test").font(.title)) {
+            Text("Blablabla")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+    
